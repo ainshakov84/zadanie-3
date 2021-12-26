faaa4282ead88d046f32b1dff7cf1c6727c69ab4
Александр Иншаков <a.inshakov84@gmail.com>
2021-12-26 20:30:42 +0300

    Добавьте файлы проекта.

---
+++ Задание урока 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_урока_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение первой вершиный x1,y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение второй вершины x2,y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение второй вершины x3,y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            int a = (x2 - x1) * (x3 - x1) + (y2 - y1) * (y3 - y1);
+            // противоположные стороны прямоуголника равны, т.е А=С и B=D
+            // условие паралельности осям выполняется если 
+
+            if (y1 == y2 & x2 == x3)
+            {
+                int x4 = x1;
+                int y4 = y3;
+                Console.WriteLine(x4);
+                Console.WriteLine(y4);
+            }
+            else
+            {
+                Console.WriteLine("Ne pravda");
+            }
+
+            Console.ReadKey();
+        }
+
+        }
+    }
+
---
+++ Задание урока 3_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_урока_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, x3, x4, y1, y2, y3, y4;
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+            x3 = int.Parse(Console.ReadLine());
+            y3 = int.Parse(Console.ReadLine());
+            y4 = x4 = 0;
+
+
+            if (x1 == x2)
+            {
+                if (y3 == y2)
+                {
+                    y4 = y1;
+                    x4 = x3;
+                }
+                else if (y3 == y1)
+                {
+                    x4 = x3;
+                    y4 = y2;
+                }
+                else Console.WriteLine("Не параллельны");
+            }
+            else if (x2 == x3)
+            {
+                if (y3 == y2)
+                {
+                    x4 = x1;
+                    y4 = y1;
+                }
+                else if (y3 == y1)
+                {
+                    x4 = x1;
+                    y4 = y2;
+                }
+                else Console.WriteLine("Не параллельны");
+            }
+            else if (x1 == x3)
+            {
+                if (y3 == y2)
+                {
+                    x4 = x2;
+                    y4 = y1;
+                }
+                else if (y3 == y1)
+                {
+                    x4 = x2;
+                    y4 = y2;
+                }
+                else Console.WriteLine("Не параллельны");
+            }
+            else Console.WriteLine("ne paralelno");
+
+
+            Console.WriteLine(x1 + "\t" + y1);
+            Console.WriteLine(x2 + "\t" + y2);
+            Console.WriteLine(x3 + "\t" + y3);
+            Console.WriteLine(x4 + "\t" + y4);
+            Console.ReadKey();
+        }
+    }
+}
